08798e16dc9af696b43533117dfe18dc7dfd454d
RizwanAhmed3000 <rizz112khan@gmail.com>
2023-11-21 12:52:38 +0500

    headerTabs

--- Components/HeaderTabs.js
+++ Components/HeaderTabs.js
@@ -1,17 +1,21 @@
 import { View, Text, TouchableOpacity } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function HeaderTabs() {
+    const [activeState, setActiveState] = useState('Delivery')
     return (
-        <View style={{ flexDirection: 'row', alignSelf: 'center' }}>
-            <HeaderButton text={'Delivery'} />
-            <HeaderButton text={'Pick up'} />
+        <View style={{ flexDirection: 'row', alignSelf: 'center', gap: 5 }}>
+            <HeaderButton text={'Delivery'} activeState={activeState} setActiveState={setActiveState} />
+            <HeaderButton text={'Pick up'} activeState={activeState} setActiveState={setActiveState} />
         </View>
     )
 }
 
-const HeaderButton = ({ text }) => (
-    <TouchableOpacity>
-        <Text>{text}</Text>
+const HeaderButton = ({ text, activeState, setActiveState }) => (
+    <TouchableOpacity
+        style={{ backgroundColor: activeState === text ? 'black' : 'white', paddingHorizontal: 16, paddingVertical: 6, borderRadius: 30, marginTop: 5 }}
+        onPress={() => setActiveState(text)}
+    >
+        <Text style={{ color: activeState === text ? 'white' : 'black', fontSize: 15, fontWeight: 900 }}>{text}</Text>
     </TouchableOpacity>
 )
--- Screens/Home.js
+++ Screens/Home.js
@@ -1,10 +1,12 @@
 import { View, Text, SafeAreaView, TouchableOpacity } from 'react-native'
+import { StatusBar } from 'expo-status-bar';
 import React from 'react'
 import HeaderTabs from '../Components/HeaderTabs'
 
 export default function Home() {
     return (
         <SafeAreaView>
+            {/* <StatusBar style="auto" /> */}
             <HeaderTabs />
         </SafeAreaView>
     )
