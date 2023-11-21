db2d1842d3c50ca841edd7ad3570ac86372d348c
RizwanAhmed3000 <rizz112khan@gmail.com>
2023-11-21 13:31:41 +0500

    format

--- Components/HeaderTabs.js
+++ Components/HeaderTabs.js
@@ -1,21 +1,43 @@
-import { View, Text, TouchableOpacity } from 'react-native'
-import React, { useState } from 'react'
+import { View, Text, TouchableOpacity } from "react-native";
+import React, { useState } from "react";
 
 export default function HeaderTabs() {
-    const [activeState, setActiveState] = useState('Delivery')
-    return (
-        <View style={{ flexDirection: 'row', alignSelf: 'center', gap: 5 }}>
-            <HeaderButton text={'Delivery'} activeState={activeState} setActiveState={setActiveState} />
-            <HeaderButton text={'Pick up'} activeState={activeState} setActiveState={setActiveState} />
-        </View>
-    )
+  const [activeState, setActiveState] = useState("Delivery");
+  return (
+    <View style={{ flexDirection: "row", alignSelf: "center", gap: 5 }}>
+      <HeaderButton
+        text={"Delivery"}
+        activeState={activeState}
+        setActiveState={setActiveState}
+      />
+      <HeaderButton
+        text={"Pick up"}
+        activeState={activeState}
+        setActiveState={setActiveState}
+      />
+    </View>
+  );
 }
 
 const HeaderButton = ({ text, activeState, setActiveState }) => (
-    <TouchableOpacity
-        style={{ backgroundColor: activeState === text ? 'black' : 'white', paddingHorizontal: 16, paddingVertical: 6, borderRadius: 30, marginTop: 5 }}
-        onPress={() => setActiveState(text)}
+  <TouchableOpacity
+    style={{
+      backgroundColor: activeState === text ? "black" : "white",
+      paddingHorizontal: 16,
+      paddingVertical: 6,
+      borderRadius: 30,
+      marginTop: 5,
+    }}
+    onPress={() => setActiveState(text)}
+  >
+    <Text
+      style={{
+        color: activeState === text ? "white" : "black",
+        fontSize: 15,
+        fontWeight: 900,
+      }}
     >
-        <Text style={{ color: activeState === text ? 'white' : 'black', fontSize: 15, fontWeight: 900 }}>{text}</Text>
-    </TouchableOpacity>
-)
+      {text}
+    </Text>
+  </TouchableOpacity>
+);
--- Screens/Home.js
+++ Screens/Home.js
@@ -1,14 +1,15 @@
-import { View, Text, SafeAreaView, TouchableOpacity } from 'react-native'
-import { StatusBar } from 'expo-status-bar';
-import React from 'react'
-import HeaderTabs from '../Components/HeaderTabs'
+import { View, Text, SafeAreaView, TouchableOpacity } from "react-native";
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import HeaderTabs from "../Components/HeaderTabs";
 
 export default function Home() {
-    return (
-        <SafeAreaView>
-            {/* <StatusBar style="auto" /> */}
-            <HeaderTabs />
-        </SafeAreaView>
-    )
+  return (
+    <SafeAreaView style={{ backgroundColor: "#eee", flex: 1 }}>
+      {/* <StatusBar style="auto" /> */}
+      <View style={{ backgroundColor: "white", padding: 10 }}>
+        <HeaderTabs />
+      </View>
+    </SafeAreaView>
+  );
 }
-
