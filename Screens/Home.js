08937a11d2b47a3549fd57e1fa6931fd83b798c3
RizwanAhmed3000 <rizz112khan@gmail.com>
2023-11-25 18:13:03 +0500

    error

--- Screens/Home.js
+++ Screens/Home.js
@@ -1,6 +1,6 @@
 import { View, Text, SafeAreaView, TouchableOpacity, ScrollView } from "react-native";
 import { StatusBar } from "expo-status-bar";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import HeaderTabs from "../Components/HeaderTabs";
 import SearchBar from "../Components/SearchBar";
 import Categories from "../Components/Categories";
@@ -8,14 +8,50 @@ import RestaurantItem from "../Components/RestaurantItem";
 import axios from "axios";
 
 export default function Home() {
+  const [city, setCity] = useState("San Francisco");
+  const [restaurantData, setRestaurantData] = useState([])
 
-  const getRestaurantsData = async () => {
-    const res = await axios.get(`/api/yelp?location=SanDiego`)
-    console.log(res)
-  };
   useEffect(() => {
-    getRestaurantsData()
-  }, [])
+    // const options = {
+    //   method: 'GET',
+    //   headers: {
+    //     accept: 'application/json',
+    //   },
+    // };
+    // fetch(`/api/yelp?location=${city}&sort_by=best_match&limit=10`, options)
+    //   .then((res) => res.json()).then((json) => setRestaurantData(json.businesses))
+    // getRestaurantsData()
+    // async function getRestaurantsData() {
+    //   const res = await axios.get(`http://localhost:8001/api/yelp?location=${city}&sort_by=best_match&limit=10`)
+    //   console.log(res)
+    //   setRestaurantData(res)
+    // }
+    // getRestaurantsData()
+    const fetchData = async () => {
+      try {
+        const options = {
+          method: 'GET',
+          headers: {
+            accept: 'application/json',
+          },
+        };
+
+        const response = await fetch(`http://192.168.3.107:8081/api/yelp?location=${city}&sort_by=best_match&limit=10`, options);
+
+        const json = await response.json();
+
+        // Filter and update state
+        setRestaurantData(
+          json.businesses.filter((business) =>
+            business.transactions.includes(activeTab.toLocaleLowerCase())
+          )
+        );
+      } catch (error) {
+        console.error("Error fetching data:", error.stack);
+      }
+    };
+    fetchData()
+  }, [city])
 
 
   return (
