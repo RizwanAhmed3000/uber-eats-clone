481c1199d0f85839ce7dd6a9ea011f50f3ced3d8
RizwanAhmed3000 <rizz112khan@gmail.com>
2023-11-25 14:34:28 +0500

    yelp api

--- Screens/Home.js
+++ Screens/Home.js
@@ -1,12 +1,23 @@
 import { View, Text, SafeAreaView, TouchableOpacity, ScrollView } from "react-native";
 import { StatusBar } from "expo-status-bar";
-import React from "react";
+import React, { useEffect } from "react";
 import HeaderTabs from "../Components/HeaderTabs";
 import SearchBar from "../Components/SearchBar";
 import Categories from "../Components/Categories";
 import RestaurantItem from "../Components/RestaurantItem";
+import axios from "axios";
 
 export default function Home() {
+
+  const getRestaurantsData = async () => {
+    const res = await axios.get(`/api/yelp?location=SanDiego`)
+    console.log(res)
+  };
+  useEffect(() => {
+    getRestaurantsData()
+  }, [])
+
+
   return (
     <SafeAreaView style={{ backgroundColor: "#eee", flex: 1 }}>
       {/* <StatusBar style="auto" StatusBarStyle="default" /> */}
